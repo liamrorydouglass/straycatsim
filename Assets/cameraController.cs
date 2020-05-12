0498cd4235ec0ead658b05dc4b2dab8eaa62c6c2
Liam Douglass <liamdouglass@liams-air.myfiosgateway.com>
2020-05-12 18:40:07 -0400

    initial commit

---
+++ Assets/cameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+	public GameObject target;
+    public float damping = 1;
+    public float shoulderAngle;
+
+    Vector3 offset;
+     
+    void Start() {
+        offset = target.transform.position - transform.position;
+    }
+     
+    void LateUpdate() {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, damping);
+         
+        Quaternion rotation = Quaternion.Euler(0, angle - shoulderAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+         
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/movement_controller.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_controller : MonoBehaviour
+{
+	CharacterController characterController;
+
+   	public float acc;
+   	public float maxSpeed = 4.0f;
+   	public float jumpForceMax = 0.0f;
+   	public float rotSpeed = 50.0f;
+   	public float sprintMod = 2f;
+
+
+   	private float haxis;
+   	private float vaxis;
+   	private float tempAcc;
+   	private bool isSprinting = false;
+
+
+
+   	private Vector3 moveDirection = Vector3.zero; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    	characterController = GetComponent<CharacterController>();
+
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+    	//if(characterController.isGrounded){
+    	checkSprint();
+        haxis = Input.GetAxis("Horizontal");
+        vaxis = Input.GetAxis("Vertical");
+    	//}
+        Debug.Log(tempAcc);
+		transform.Rotate(0.0f, haxis * Time.deltaTime * rotSpeed, 0.0f);
+		transform.Translate(-vaxis * Time.deltaTime * tempAcc, 0.0f, 0.0f);
+			
+	}
+
+	void checkSprint(){
+		if(Input.GetKey(KeyCode.LeftShift)){
+			isSprinting = true;
+			tempAcc = acc * sprintMod;
+		}
+		else{
+			isSprinting = false;
+			tempAcc = acc;
+		}
+	}
+}
+
+/*
+using UnityEngine;
+using System.Collections;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
+
+public class movement_controller : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}*/
